8b94a409a729ea165c427cb054917c6796263e86
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-15 15:11:57 -0300

    Update 1.1.py

--- aula5/1.1.py
+++ aula5/1.1.py
@@ -2,4 +2,20 @@
 Este script mostra qual caractere mais aparece na string
 """
 
-string = 'O rato roeu a roupa do rei de roma'
+minha_string = 'O rato roeu a roupa do rei de roma'
+tamanho_string = len(minha_string)
+
+c = 0
+contagem = 0
+letra = ''
+while c < tamanho_string:
+    conta = minha_string.count(minha_string[c])
+
+    if contagem < conta and minha_string[c].split():
+        letra = minha_string[c]
+
+        contagem = conta
+
+    c += 1
+
+print(letra, contagem)
