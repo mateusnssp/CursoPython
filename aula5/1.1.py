1fc78e5059cd684cb2ca388c5622f5d5fe2a0f99
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-15 11:07:48 -0300

    Update 1.1.py

--- aula5/1.1.py
+++ aula5/1.1.py
@@ -4,6 +4,9 @@ Este script mostra qual caractere mais aparece na string
 
 string = 'O rato roeu a roupa do rei de roma'
 
+comparador = 1
 
 for caractere in string:
+    if string.count(caractere) > comparador:
+        comparador = string.count(caractere)
     print(caractere, string.count(caractere))
