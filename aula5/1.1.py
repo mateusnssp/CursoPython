eb6b8a57b68298e8cc5a3e70d1b473500b2ecb73
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-15 10:40:33 -0300

    aula5

---
+++ aula5/1.1.py
@@ -0,0 +1,9 @@
+"""
+Este script mostra qual caractere mais aparece na string
+"""
+
+string = 'O rato roeu a roupa do rei de roma'
+
+
+for caractere in string:
+    print(caractere, string.count(caractere))
