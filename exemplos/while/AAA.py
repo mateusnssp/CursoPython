b059cd018b6c2807e0a99c28e6d2aa509bb8bffb
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-15 19:52:37 -0300

    organização

---
+++ exemplos/while/AAA.py
@@ -0,0 +1,28 @@
+"""
+Este script ativa .upper() em todas as letras após um espaço.
+
+DESAFIO: corrigir erro: primeiro caractere sempre fica em sua forma original.
+"""
+
+while True:
+
+    minha_string = str(input("Digite uma frase: "))
+    nova_string = ''
+
+    c = 0
+    tamanho_da_string = len(minha_string)
+
+    while c < tamanho_da_string:
+        if minha_string[c] == ' ':
+            nova_string = nova_string + minha_string[c]
+            nova_string = nova_string + minha_string[c + 1].upper()
+        else:
+            if minha_string[c - 1] == ' ':
+                pass
+            else:
+                nova_string = nova_string + minha_string[c]
+
+        print(nova_string)
+        c += 1
+
+    print(nova_string)
