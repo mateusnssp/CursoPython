f50fc66658163ce09cacd7a04de0a7724de87afa
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-15 20:08:06 -0300

    Update AAA.py

---
+++ exemplos/for/AAA.py
@@ -0,0 +1,22 @@
+
+
+### Gera lista ordenada
+l, num = [], 0
+while num < 11:
+    l.append(num)
+    num += 1
+print(l)
+
+
+
+
+lista_num_pares = []
+for num in l:
+    if num % 2 == 0:
+        lista_num_pares.append(num)
+
+print(lista_num_pares)
+
+
+
+
