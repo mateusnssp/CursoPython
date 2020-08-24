2f70aca877943bdbf0d90542661521ec9834cf64
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-24 10:45:46 -0300

    add configs.py ; update 'listas'
    configs.py ajuda ao autor a numerar os índices de amostras

---
+++ configs.py
@@ -0,0 +1,12 @@
+
+class Configure():
+    def __init__(self, values):
+        pass
+
+    def generate(value):
+        string = []
+        for i in list(range(1, value + 1)):
+            string.append('i.{}'.format(i))
+        return string
+
+ss = '{} : {}'
--- listas/B-AAA.py
+++ listas/B-AAA.py
@@ -1,23 +1,28 @@
-"""Manipulação de listas"""
+from configs import *
+s = Configure.generate(5)
 
+"""Manipulação de listas"""
 
 lista =    [7, 5, 6, 2, 3, 1, 8, 4, 9, 0]
 # índices:  0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
-"""Alteração dos respectivos índices"""
+"""{i.1} Alteração dos respectivos índices"""
 lista[0] = 3
 lista[0] = lista[2]
+lista[5] = lista[-3]
+
+print(ss.format(s[0], lista))
 
 """
 Fatiamento:
 list[start = 0 : end = 0 : step = 1]
 """
-lista_fatiada = [lista[:5]]
-lista_fatiada2 = [lista[5:]]
-lista_fatiada3 = [lista[3:8]]
-lista_fatiada4 = [lista[0::2]]
+lista_fatiada = [lista[:5]]              #{i.2}
+print(ss.format(s[1], lista_fatiada))
+lista_fatiada2 = [lista[5:]]             #{i.3}
+print(ss.format(s[2], lista_fatiada2))
+lista_fatiada3 = [lista[3:8]]            #{i.4}
+print(ss.format(s[3], lista_fatiada3))
+lista_fatiada4 = [lista[0::2]]           #{i.5}
+print(ss.format(s[4], lista_fatiada4))
 
-print(lista_fatiada)
-print(lista_fatiada2)
-print(lista_fatiada3)
-print(lista_fatiada4)
