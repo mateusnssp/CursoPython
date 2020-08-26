93c88a32d0d4f7f4d830fe11fe3df080ee9aa727
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-26 13:38:33 -0300

    AAF

---
+++ fluxo/if/A-AAF.py
@@ -0,0 +1,15 @@
+Try = True
+while Try:
+    age = input('Informe sua idade: ')
+    if not age.isnumeric():
+        msg = 'Por gentileza, digite apenas números.'
+        Try = True
+    else:
+        msg = 'Sua idade é {}.'.format(age)
+        Try = False
+    print(msg)
+    if Try == True:
+        msg2 = 'true'
+    else:
+        msg2 = 'false'
+    print('Repetir: {}'.format(msg2))
