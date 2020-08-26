cbc1d86500c9a7916c3615e3791beba0052cb009
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-26 17:44:55 -0300

    Sets

--- conjuntos/B-AAG.py
+++ conjuntos/B-AAG.py
@@ -12,3 +12,15 @@ def i3(num):
     return num in s
 print(ss.format(ind[20], i3(4))) #{i..2.0}
 print(ss.format(ind[21], i3(8))) #{i..2.1}
+
+s = set('54632877565')
+print(ss.format(ind[30], s))     #{i..3.0}
+
+"""{i..4.0} Operações"""
+a = set('beija-flor')
+b = {'m', 'a', 't', 'e', 'u', 's'}
+print(ss.format(ind[40], 'a: {},\n{} b: {}'.format(a, ' '*(len(ind[40]) + ss.count(' ')), b)))
+print(ss.format(ind[41], a - b))
+print(ss.format(ind[42], a | b))
+print(ss.format(ind[43], a & b))
+print(ss.format(ind[44], a ^ b))
