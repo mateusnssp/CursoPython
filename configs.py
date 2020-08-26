28df6652c3413546c3c660489a42643e4f7e2005
Mateus Nunes <mateusnss@protonmail.ch>
2020-08-26 19:56:06 -0300

    Descrição

--- configs.py
+++ configs.py
@@ -1,26 +1,22 @@
-class Configure():
-    """
-    Esta classe automatiza a produção dos índices para as amostras através do .print().
-    É necessário chamar o método Configure.generate() para uma variável type <class 'list'>.
-    """
-    def __init__(self):
-        pass
-
-    def generate(self = None):
-        """.generate(quantidade de índices necessários)"""
-        lim, quantidade = list(range(10)), range(10)
-        string, vl, svl, ssvl = [], lim, lim, lim
-        ns = '0'
-        for iq in quantidade:
-            for ivl in vl:
-                for isvl in svl:
-                    for issvl in ssvl:
-                        string.append('i.{}.{}.{}'.format(ns if ivl == 0 else ivl, ns if isvl == 0 else isvl, issvl))
-        return string
+"""
+Automatiza a produção dos índices para as amostras de etapas de execução.
+ss(índice, descrição, subdescrição)
+"""
+def generate(self = None):
+    """.generate(quantidade de índices necessários)"""
+    lim, quantidade = list(range(10)), range(10)
+    string, vl, svl, ssvl = [], lim, lim, lim
+    ns = '0'
+    for iq in quantidade:
+        for ivl in vl:
+            for isvl in svl:
+                for issvl in ssvl:
+                    string.append('i.{}.{}.{}'.format(ns if ivl == 0 else ivl, ns if isvl == 0 else isvl, issvl))
+    return string
 
 def ss(ind, desc, subDesc = ''):
 
     ss = '{} : {} - {}.' if not subDesc == '' else '{} : {}.'
 
-    indi = Configure.generate()
+    indi = generate()
     return print(ss.format(indi[ind], str(desc), str(subDesc)))
