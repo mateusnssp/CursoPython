8cb35e7277794312b4a7543344ae1266421822ae
mateusnssp <mateusnss@protonmail.ch>
2020-09-05 09:54:09 -0300

    configs.py

---
+++ classes/pessoa.py
@@ -0,0 +1,29 @@
+class Pessoa():
+
+    def __init__(self, nome, idade, falando=False, comendo=False):
+        self.nome = nome
+        self.idade = idade
+        self.falando = falando
+        self.comendo = comendo
+
+    def apresentar(self):
+        self.falar = True
+        if not self.comendo:
+            print(f'Oi, meu nome é {self.nome} e tenho {self.idade} anos de idade.')
+        else:
+            print('f{nome} não pode se apresentar pois está comendo.')
+
+    def comer(self):
+        self.comendo = True
+        if not self.falar:
+            print(f'{self.nome} está comendo')
+        else:
+            print(f'{self.nome} não pode comer pois está falando.')
+
+
+
+
+
+if __name__ == '__main__':
+    test = Pessoa('Teste', 1)
+    test.apresentar()
--- configs.py
+++ configs.py
@@ -1,22 +1,12 @@
-"""
-Automatiza a produção dos índices para as amostras de etapas de execução.
-ss(índice, descrição, subdescrição)
-"""
-def generate(self = None):
-    """.generate(quantidade de índices necessários)"""
-    lim, quantidade = list(range(10)), range(10)
-    string, vl, svl, ssvl = [], lim, lim, lim
-    ns = '0'
-    for iq in quantidade:
-        for ivl in vl:
-            for isvl in svl:
-                for issvl in ssvl:
-                    string.append('i.{}.{}.{}'.format(ns if ivl == 0 else ivl, ns if isvl == 0 else isvl, issvl))
-    return string
+def ss(indice=0, desc='', subDesc=''):
+    """
+    Estrutura  ::    i.n1.n2.n3 : descrição - descrição segunda
+    Variáveis  ::   'i'.n1.n2.n3 :   desc   -     subDesc
+    """
+    listaBase = list(range(10))
+    listaString = []
+    l = [listaString.append(f'i.{n1}.{n2}.{n3}') for n1 in listaBase for n2 in listaBase for n3 in listaBase]
 
-def ss(ind, desc, subDesc = ''):
-
-    ss = '{} : {} - {}.' if not subDesc == '' else '{} : {}.'
-
-    indi = generate()
-    return print(ss.format(indi[ind], str(desc), str(subDesc)))
+    strDesc = ": {}".format(desc) if desc != '' else ''
+    strSubDesc = "- {}".format(subDesc) if subDesc != '' else ''
+    print("{} {} {}".format(listaString[indice], strDesc, strSubDesc))
